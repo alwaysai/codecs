97d2e93c3bb6c954764bcf91f8fec090736fbce9
Chris Arnesen <chris@alwaysai.co>
2019-04-16 10:55:47 -0700

    Ooops forgot to hit save

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import * as t from 'io-ts';
 
 export { cast } from './cast';
 
+export { emptyArray } from './empty-array';
+export { optionalString } from './optional-string';
 export const nullableString = t.union([t.string, t.null], 'nullableString');
 export { uuid } from './uuid';
 export { systemId } from './system-id';
-
-export { optionalString } from './optional-string';
-export { emptyArray } from './empty-array';
